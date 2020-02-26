8e76e6b7ddddba0e408cecd07ada1f6eb9c2a3c8
ILS\motorin <yuramot@mail.ru>
2020-02-26 14:23:06 +0300

    compile kafka

--- src/Kafka/IlsKafkaBackup.dpr
+++ src/Kafka/IlsKafkaBackup.dpr
@@ -3,16 +3,16 @@ program IlsKafkaBackup;
 uses
   Vcl.Forms,
   UMain in 'UMain.pas' {Form3},
-  Ils.Kafka in '..\_Common\Ils.Kafka.pas',
-  Ils.Kafka.Conf in '..\_Common\Ils.Kafka.Conf.pas',
-  ULibKafka in '..\..\lib\Kafka\ULibKafka.pas',
-  JsonDataObjects in '..\..\lib\Json\JsonDataObjects.pas',
-  Ils.MySql.Conf in '..\_Common\Ils.MySql.Conf.pas',
-  Ils.Json.Utils in '..\_Common\Ils.Json.Utils.pas',
-  Ils.Utils in '..\_Common\Ils.Utils.pas',
-  Ils.Json.Names in '..\_Common\Ils.Json.Names.pas',
-  Ils.Logger in '..\_Common\Ils.Logger.pas',
-  CINIFilesData in '..\_Common\CINIFilesData.pas';
+  Ils.Kafka in '..\..\..\IlsSaaSCommon\_Common\Ils.Kafka.pas',
+  Ils.Kafka.Conf in '..\..\..\IlsSaaSCommon\_Common\Ils.Kafka.Conf.pas',
+  ULibKafka in '..\..\..\IlsSaaSCommon\lib\Kafka\ULibKafka.pas',
+  JsonDataObjects in '..\..\..\IlsSaaSCommon\lib\Json\JsonDataObjects.pas',
+  Ils.MySql.Conf in '..\..\..\IlsSaaSCommon\_Common\Ils.MySql.Conf.pas',
+  Ils.Json.Utils in '..\..\..\IlsSaaSCommon\_Common\Ils.Json.Utils.pas',
+  Ils.Utils in '..\..\..\IlsSaaSCommon\_Common\Ils.Utils.pas',
+  Ils.Json.Names in '..\..\..\IlsSaaSCommon\_Common\Ils.Json.Names.pas',
+  Ils.Logger in '..\..\..\IlsSaaSCommon\_Common\Ils.Logger.pas',
+  CINIFilesData in '..\..\..\IlsSaaSCommon\_Common\CINIFilesData.pas';
 
 {$R *.res}
 
